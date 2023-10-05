78cf1ceab35172f51439bb266b61da0a88138942
Sindre Tupsjøen <sintup@hotmail.no>
2023-10-05 14:54:03 +0200

    !A Menu

---
+++ Assets/Scripts/Flipper.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    [SerializeField] private AnimationCurve animationCurve;
+    private float _timer;
+    private Rigidbody _rb;
+
+    private Quaternion _startRot;
+    private void Awake()
+    {
+        _startRot = transform.rotation;
+        _rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _timer = 0;
+        }
+        _timer += Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        // Rotatsjon logik
+        // Instead of rotating object using transform.rotation / transform.localEulerAngles
+        // we use rigidbody.MoveRotation. This rotates the object in the physicsstep (Fixed Update) and ensures 
+        // other raindrops / pinballs dont clip through and receives velocity!
+
+        _rb.MoveRotation(_startRot * Quaternion.Euler(0, 0, animationCurve.Evaluate(_timer) * 90f));
+
+    }
+
+}
